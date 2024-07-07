ceb6dd7557f1545eb5895f1e2f6ca8a1b0d2f3a9
Ryzvelt <ryzhckinp@yandex.ru>
2024-07-07 11:07:14 +0300

    Update module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -1,14 +1,15 @@
 def generate_password(n):
-    result = ""
+result = ""
+for i in range(1, n):
+for j in range(i+1, n):
+if (i + j) % n == 0:
+result += str(i) + str(j)
+return result
 
-    for i in range(1, 21):
-        for j in range(i+1, 21):  
-            if (i + j) % n == 0:  
-                result += str(i) + str(j)
-    return result
-number = int(input("Введите число для пароля от 3 до 20: "))
+number = 0
 while number < 3 or number > 20:
-    number = int(input("Некорректный ввод. Введите число от 3 до 20: "))
+number = int(input("Введите число для пароля от 3 до 20: "))
+
 password = generate_password(number)
 print(f"Пароль для числа {number}: {password}")
-print("Поздравляю, вы сбежали!")
+print("Поздравляю,вы сбежали") 
