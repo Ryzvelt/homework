194a8fded21a113da98a609e4e859b67ee345fdd
Ryzvelt <ryzhckinp@yandex.ru>
2024-12-13 05:35:21 +0300

    Update module_7_3.py

--- module_7_3.py
+++ module_7_3.py
@@ -1,30 +1,44 @@
-# Входные данные
-team1_num = 5
-team2_num = 6
-score_1 = 40
-score_2 = 42
-team1_time = 1552.512
-team2_time = 2153.31451
-tasks_total = 82
-time_avg = 45.2
+import string
 
-# Определение результата соревнования
-if score_1 > score_2 or (score_1 == score_2 and team1_time < team2_time):
-    challenge_result = 'Победа команды Мастера кода!'
-elif score_1 < score_2 or (score_1 == score_2 and team1_time > team2_time):
-    challenge_result = 'Победа команды Волшебники данных!'
-else:
-    challenge_result = 'Ничья!'
+class WordsFinder:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.words = self.get_all_words()
 
-# Использование % для форматирования строк
-print("В команде Мастера кода участников: %d !" % team1_num)
-print("Итого сегодня в командах участников: %d и %d !" % (team1_num, team2_num))
+    def get_all_words(self):
+        with open(self.file_name, 'r', encoding='utf-8') as file:
+            content = file.read().lower()
+            content = content.translate(str.maketrans('', '', string.punctuation))
+            words = content.split()
+        return words
 
-# Использование format() для форматирования строк
-print("Команда Волшебники данных решила задач: {} !".format(score_2))
-print("Волшебники данных решили задачи за {:.2f} с !".format(team2_time))
+    def find(self, word):
+        """Находит слово и возвращает его порядковый номер (1-индексация) в тексте."""
+        word = word.lower()
+        try:
+            index = self.words.index(word) + 1  # 1-индексация
+            return {self.file_name: index}
+        except ValueError:
+            return {self.file_name: None}  # Если слово не найдено
 
-# Использование f-строк для форматирования строк
-print(f"Команды решили {score_1} и {score_2} задач.")
-print(f"Результат битвы: {challenge_result}")
-print(f"Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!.")
+    def count(self, word):
+        """Считает количество вхождений слова в тексте."""
+        word = word.lower()
+        total_count = self.words.count(word)
+        return {self.file_name: total_count}
+
+# Функция для создания файла с примерным содержимым
+def create_test_file():
+    with open('test_file.txt', 'w', encoding='utf-8') as file:
+        content = """It's a text for task. Найти везде, используйте его для самопроверки. 
+        Успехов в решении задачи. Text text text."""
+        file.write(content)
+
+# Создаем тестовый файл
+create_test_file()
+
+# Пример использования
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words())  # Все слова
+print(finder2.find('TEXT'))      # 3 слово по счёту
+print(finder2.count('teXT'))     # 4 слова teXT в тексте всего
